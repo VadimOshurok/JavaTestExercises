d4aaa99b822578c37599906b4f571f7cd168e206
I'am <erghostik@gmail.com>
2019-04-12 15:04:47 +0300

    d

--- Calculator.java
+++ Calculator.java
@@ -11,7 +11,7 @@ public class Calculator {
 			char operation = getOperation();
 			double result = calc(num1,num2,operation);
 			System.out.println("Результат операции: "+result); 
-            System.out.println("Хотите продолжить 1.Yes/2.No"); 
+            System.out.println("Хотите продолжить  1.Yes/2.No"); 
             int input = scanner.nextInt();
 			if(input!=1) {
 				continueCalculations=false;
