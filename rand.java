32451bf5e49455f1db7d1620b6194342f5c91918
I'am <erghostik@gmail.com>
2019-04-15 15:47:06 +0300

    Main Menu

--- rand.java
+++ rand.java
@@ -1,31 +1,31 @@
+package testMenu;
 import java.util.Scanner;
+public class PlayWithRandom { 
+Scanner scanner = new Scanner(System.in);
 
-public class rand {
-	static Scanner scanner = new Scanner(System.in);
-	
-	public static void main(String[] args) {
-		 boolean continueCalculations=true; 
+	public void startGame() {
+		boolean continueCalculations=true; 
 			do {
 				System.out.println("Вам надо отгдадать число от 1 до 10");
-				int num = getInt();
+				int num = getInt2();
 				int rand = randomGenerator();
 				body(num, rand); 
-	            System.out.println("Хотите попробывать еще раз? 1.Yes/2.No"); 
-	            int input = scanner.nextInt();
+				System.out.println("Хотите попробывать еще раз? 1.Yes/2.No"); 
+				int input = scanner.nextInt();
 				if(input!=1) {
 					continueCalculations=false;
-	            }
+				}
 			}
 			while(continueCalculations);
 	}
-	
-	public static int randomGenerator() {
+
+	public int randomGenerator() {
 		int rand = 1 + (int) (Math.random()*10);
 		System.out.println("Программа загадал число: " + rand);
 		return rand;
 	}
-	
-	public static int getInt() {
+
+	public int getInt2() {
 		int num;
 		System.out.println("Введите число: ");
 		if (scanner.hasNextInt()) {
@@ -34,18 +34,19 @@ public class rand {
 		else {
 			System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
 			scanner.next();
-			num = getInt();
+			num = getInt2();
 		}
 		return num;
 	}
+	
 	public static int body(int rand, int num){
-		if (rand == num) {
-			System.out.println("Вы угадали число! ");
-		}
-		else {
-			System.out.println("Число не совпало. ");
-		}
-		return num;
+			if (rand == num) {
+				System.out.println("Вы угадали число! ");
+			}
+			else {
+				System.out.println("Число не совпало. ");
+			}
+			return num;
 	}
 
 }
