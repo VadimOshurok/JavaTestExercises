f7d87bcb0eda00d1af3945d646c058cd7eba710e
VadimOshurok <49399798+VadimOshurok@users.noreply.github.com>
2019-04-17 10:55:57 +0300

    Update Massive.java

--- Massive.java
+++ Massive.java
@@ -7,37 +7,36 @@ public class Massive {
 	public void startMassive() {
 		boolean continueCalculations=true; 
 		do {
-
 			massive();
-            System.out.println("Хотите продолжить 1.Yes/2.No"); 
-            int input = scanner.nextInt();
+           		System.out.println("Хотите продолжить 1.Yes/2.No"); 
+            		int input = scanner.nextInt();
 			if(input!=1) {
 				continueCalculations=false;
-            }
+        		}
 		}
 		while(continueCalculations);	 
 		}	
 	
 	public int massive(){
 	
-			ArrayList<Integer> numbers = new ArrayList<Integer>();
-			int total=0;
-			boolean continueCalculations=true; 
-			do {	
-				System.out.println("Введите число: ");
-				double num = 0.0;
-				if (scanner.hasNextInt()) {
-					num = scanner.nextInt();
-				}
-				numbers.add((int) num);
-				total += num;
-				if(num==0) {
-					continueCalculations=false;	
-	            }
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		int total=0;
+		boolean continueCalculations=true; 
+		do {	
+			System.out.println("Введите число: ");
+			double num = 0.0;
+			if (scanner.hasNextInt()) {
+				num = scanner.nextInt();
 			}
-			while(continueCalculations);	
-			System.out.println(numbers);
-			System.out.println("Сумма чисел составила: "+ total);
+			numbers.add((int) num);
+			total += num;
+			if(num==0) {
+				continueCalculations=false;	
+			}
+		}
+		while(continueCalculations);	
+		System.out.println(numbers);
+		System.out.println("Сумма чисел составила: "+ total);
 		return total;	
 		}
 }
